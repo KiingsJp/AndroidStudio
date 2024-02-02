c60c62a0f833871bd1e585d8118bf3ad3fec9452
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-02 18:26:01 -0300

    Create ListView.kt

---
+++ ListView.kt
@@ -0,0 +1,25 @@
+package com.kings.study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var listViewTeste: ListView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        listViewTeste = findViewById(R.id.list_view)
+        listViewTeste.adapter = ArrayAdapter(
+
+            this,
+            android.R.layout.simple_list_item_1,
+            listOf("Elemento 1", "Elemento 2", "Elemento 3", "Elemento 4", "Elemento 4")
+        )
+    }
+}
