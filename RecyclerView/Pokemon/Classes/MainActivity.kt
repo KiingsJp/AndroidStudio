7e9f72f0265d408db15d7c6ff4a2262eaf55740a
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-07 20:09:15 -0300

    Add files via upload

---
+++ RecyclerView/Pokemon/Classes/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.kings.pokedex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var pokedexView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        pokedexView = findViewById(R.id.rv_pokedex)
+        pokedexView.adapter = PokedexAdapter(createPokemons())
+        pokedexView.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun createPokemons(): List<Pokemon> {
+        return listOf(
+            Pokemon(
+                "Bulbasaur",
+                "Grass",
+                R.drawable.bulbasaur,
+                "Ivysaur"
+            ),
+            Pokemon(
+                "Charmander",
+                "Fire",
+                R.drawable.charmander,
+                "Charmeleon"
+            ),
+            Pokemon(
+                "Squirtle",
+                "Water",
+                R.drawable.squirtle,
+                "Wartortle"
+            )
+        )
+    }
+}
---
+++ RecyclerView/Pokemon/Classes/Pokemon.kt
@@ -0,0 +1,7 @@
+package com.kings.pokedex
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import android.widget.TextView
+
+data class Pokemon(val nomePokemon: String, val tipoPokemon: String, val imgPokemon: Int, val evolucaoPokemon: String)
