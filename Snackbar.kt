2a5fcad5936e80913ee4f3c270813146abed5788
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-19 16:56:35 -0300

    Add files via upload

---
+++ Snackbar.kt
@@ -0,0 +1,24 @@
+package com.kings.project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import com.kings.project.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activityMainBinding.root)
+
+        with(activityMainBinding){
+            btnExecutar.setOnClickListener{
+                Snackbar.make(it, "Snack Bar", Snackbar.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
