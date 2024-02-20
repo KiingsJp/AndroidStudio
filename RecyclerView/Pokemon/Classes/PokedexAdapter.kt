da1cfca4d6f2df8b932717315e7bcd73e7a185b3
Jp <117123876+KiingsJp@users.noreply.github.com>
2024-02-20 20:34:14 -0300

    Update PokedexAdapter.kt

--- RecyclerView/Pokemon/Classes/PokedexAdapter.kt
+++ RecyclerView/Pokemon/Classes/PokedexAdapter.kt
@@ -8,12 +8,14 @@ import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.kings.pokedex.databinding.CardPokemonBinding
 
-class PokedexAdapter( private val clique: (pokemon: Pokemon)-> Unit) : Adapter<PokedexAdapter.PokedexAdapterHolder>() {
+class PokedexAdapter(private val clique: (pokemon: Pokemon) -> Unit) :
+    Adapter<PokedexAdapter.PokedexAdapterHolder>() {
 
     private var pokemons = mutableListOf<Pokemon>()
 
-    fun atualizarLista(lista: MutableList<Pokemon>){
+    fun atualizarLista(lista: MutableList<Pokemon>) {
         pokemons.addAll(lista)
 
 //         ATUALIZA O ITEM PELA POSICAO INSERIDA
@@ -26,35 +28,35 @@ class PokedexAdapter( private val clique: (pokemon: Pokemon)-> Unit) : Adapter<P
 //        notifyDataSetChanged()
     }
 
-    // PARA O VIEWBINDING, UTILIZE:
-    // inner class PokedexAdapterHolder(val binding: CardPokemonBinding) : ViewHolder( binding.root ) { binding.nomePokemon.text = pokemon.nomePokemon  }
+    inner class PokedexAdapterHolder(binding: CardPokemonBinding) : ViewHolder(binding.root) {
+//    inner class PokedexAdapterHolder(view: View) : ViewHolder(view) {
 
-    inner class PokedexAdapterHolder(view: View) : ViewHolder(view) {
-        private val nomePokemon: TextView = view.findViewById(R.id.text_nome)
-        private val tipoPokemon: TextView = view.findViewById(R.id.text_tipo)
-        private val imgPokemon: ImageView = view.findViewById(R.id.img_pokemon)
-        private val evolucaoPokemon: TextView = view.findViewById(R.id.text_evolucao)
-        private val cardView: CardView = view.findViewById(R.id.cardv_pokemon)
+        private val nomePokemon: TextView = binding.textNome            // view.findViewById(R.id.text_nome)
+        private val tipoPokemon: TextView = binding.textTipo            // view.findViewById(R.id.text_tipo)
+        private val imgPokemon: ImageView = binding.imgPokemon          // view.findViewById(R.id.img_pokemon)
+        private val evolucaoPokemon: TextView = binding.textEvolucao    // view.findViewById(R.id.text_evolucao)
+        private val cardView: CardView = binding.cardvPokemon           // view.findViewById(R.id.cardv_pokemon)
 
-        fun bind( pokemon : Pokemon ) {
+        fun bind(pokemon: Pokemon) {
+
             nomePokemon.text = pokemon.nomePokemon
             imgPokemon.setImageResource(pokemon.imgPokemon)
             tipoPokemon.text = pokemon.tipoPokemon
             evolucaoPokemon.text = pokemon.evolucaoPokemon
 
             cardView.setOnClickListener {
-               clique(pokemon)
+                clique(pokemon)
             }
         }
     }
 
-    // PARA O VIEWBINDING, UTILIZE:
-    // onCreateViewHold(parent: ViewGroup, viewType: Int): PokedexAdapterHolder { val binding = CardPokemonBinding.inflate( layoutInflater, parent, false); return PokedexAdapterHolder(binding); }
- 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokedexAdapterHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.card_pokemon, parent, false)
-        return PokedexAdapterHolder(view)
+//        val view = layoutInflater.inflate(R.layout.card_pokemon, parent, false)
+//        return PokedexAdapterHolder(view)
+
+        val binding = CardPokemonBinding.inflate(layoutInflater, parent, false)
+        return PokedexAdapterHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PokedexAdapterHolder, position: Int) {
